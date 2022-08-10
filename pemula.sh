5eafa2a03835a4dd75f7c6fcda93c527fb38d2cd
Mr Sadboy01 <110970469+MR-SADBOY01@users.noreply.github.com>
2022-08-11 04:20:37 +0700

    Add files via upload

---
+++ pemula.sh
@@ -0,0 +1,100 @@
+#!/bin/sh/
+# by MR SADBOY01
+# JANGAN DI RECODE ANJING
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f big -F gay "WELCOME"
+sleep 1
+echo
+echo "======================================================================" | lolcat
+echo $red"[•] Author : MR SADBOY01             [•]"
+echo $green"[•] Team   : BLACKHAT SAMUDERA TEAM  [•]"
+echo $yellow"[•] Ig     : mr_sadboy01             [•]"
+echo "======================================================================" | lolcat
+echo $purple"           SELAMAT DATANG DI SCRIPT KU ^_^"
+echo "======================================================================" | lolcat
+echo
+echo $cyan   "1).Alqur'an"
+echo "======================" | lolcat
+echo $purple  "2).Virtex"
+echo "======================" | lolcat
+echo $blue     "3).Wa immune"
+echo "======================" | lolcat
+echo $green     "4).About"
+echo "======================" | lolcat
+echo $red       "5).Keluar"
+echo "======================" | lolcat
+echo $yellow    "6).Lapor Bag"
+echo "======================" | lolcat
+echo
+sleep 1
+read -p "========> " contoh
+if [ $contoh = 1 ]
+then
+pkg update && pkg upgrade
+pkg install bash
+pkg install git
+read -p "enter" yah
+git clone https://github.com/MR-SADBOY01/amma.sh
+read -p "enter" bacot
+cd amma.sh
+sh amma.sh
+fi
+if [ $contoh = 2 ]
+then
+pkg update && pkg upgrade
+   pkg install python
+   pkg install mv
+   pkg install git
+   git clone https://github.com/MR-X-Junior/Virtex
+   read -p "enter " yah
+   cd Virtex
+   read -p "enter " bacot
+   python3 Virtex.py
+fi
+if [ $contoh = 3 ]
+then
+sleep 1
+echo
+echo $red"password : diam"
+     xdg-open https://www.mediafire.com/file/vkzmpt8qxj76csz/com.whatsapp.zip/file?dkey=yvamq9htgfo&r=1
+echo
+fi
+if [ $contoh = 4 ]
+then
+sleep 1
+echo
+echo $red"                     info script"
+echo "======================================================================" | lolcat
+echo $green"           [●] Author    : MR SADBOY01                   [●]"
+echo $red"           [●]Team       : Blackhat Samudera Team        [●]"
+echo $cyan"           [●]Script     : Campuran                      [●]"
+echo $yellow"           [●]Update     : 10-07-2022 20:31 wib          [●]"
+echo $purple"           [●]Version    : 1.0                           [●]"
+echo "======================================================================" | lolcat
+sleep 1
+echo
+echo $white"                   Contack Me ^_^"
+echo "======================================================================" | lolcat
+echo $red"   [●]Github  : https://github.com/MR-SADBOY01  [●]"
+echo $blue"   [●] Wa     : 6281261037298                   [●]"
+echo $green "  [●] ig     : mr_sadboy01                     [●]"
+echo "======================================================================" | lolcat
+fi
+if [ $contoh = 5 ]
+then
+sleep 1
+      xdg-open https://youtube.com/channel/UCa0RlpVoScKQDIcMbtobmQQ
+fi
+if [ $contoh = 6 ]
+then
+sleep 1
+      xdg-open https://wa.me/6281261037298
+fi
